83666ef2b498b162e2adc68529e308810d635bdc
BereczkiA <bereczki.attila@ipsystems.hu>
2021-11-13 20:06:34 +0100

    3 Alap angular

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Photo
+# MyApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.1.3.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to photo!');
+    expect(page.getTitleText()).toEqual('Welcome to myApp!');
   });
 });
